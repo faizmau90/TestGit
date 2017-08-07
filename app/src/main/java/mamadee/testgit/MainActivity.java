717170d1ce33bc1de8736fae0ce7677b9bf552af
faizmau90 <faizmau90@gmail.com>
2017-08-07 23:28:50 +0530

    thanks

--- app/src/main/java/mamadee/testgit/MainActivity.java
+++ app/src/main/java/mamadee/testgit/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("Faiz Alam");
+        System.out.println("Faiz Alam");
     }
 }
